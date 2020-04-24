44f0ccfdd9051c328c146bcbab1b832b7033aba1
gonpuga <gonpuga@gmail.com>
2020-04-24 11:19:25 +0200

    Bound Service Example - Chronometer

--- app/src/main/java/com/example/serviciocronometro/MainActivity.java
+++ app/src/main/java/com/example/serviciocronometro/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.serviciocronometro;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView timestampText = (TextView) findViewById(R.id.timestamptext);
-        Button btnPrintTimeStamp = (Button) findViewById(R.id.btnPrintTimeStamp);
-        Button btnStopService = (Button) findViewById(R.id.btnStopService);
+        final TextView timestampText = findViewById(R.id.timestamptext);
+        Button btnPrintTimeStamp = findViewById(R.id.btnPrintTimeStamp);
+        Button btnStopService = findViewById(R.id.btnStopService);
         btnPrintTimeStamp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
