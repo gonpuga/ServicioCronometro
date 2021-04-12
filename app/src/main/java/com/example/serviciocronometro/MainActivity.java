b1da11de93e72e6c4d8bf16813dac621b565bfaa
gonpuga <gonpuga@gmail.com>
2021-04-12 12:19:33 +0200

    Bound Service Example - Chronometer

--- app/src/main/java/com/example/serviciocronometro/MainActivity.java
+++ app/src/main/java/com/example/serviciocronometro/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     BoundService mBoundService;
     boolean mServiceBound = false;
